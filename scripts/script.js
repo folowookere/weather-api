68d870bd34e13de96ce72e9bacef0b1ba9df2697
Funmi <folowookere@yahoo.com>
2018-05-24 20:25:38 -0400

    Revised HTML & CSS

--- scripts/script.js
+++ scripts/script.js
@@ -31,15 +31,16 @@ $(document).ready(function() {
         var description = data.weather[0].description;
         var weathericon = data.weather[0].icon;
         var temperatureF = Math.round(temperature * (9/5)) + 32;
-        $("#temperatureF").hide();
         //To make sure conversion is working
          console.log(temperatureF);
          
         $("#city").html(city);
-        $("#temperature").html(temperature + " C");
         $("#temperatureF").html(temperatureF + " F");
+        $("#temperature").html(temperature + " C");
         $("#description").html(description);
         $("#weathericon").attr("src", weathericon);
+        $("#temperature").hide();
+
         
            $("#toggle").click(function() {
                 $("#temperature,#temperatureF").toggle();
@@ -49,24 +50,22 @@ $(document).ready(function() {
                 
               if(Math.round(temperature * (9/5)) + 32 > -20 && Math.round(temperature * (9/5)) + 32 <= 32) {
                 color='linear-gradient(-90deg, #56ccf2, #2f80ed)'
-              } else if (Math.round(temperature * (9/5)) + 32 > 32 && Math.round(temperature * (9/5)) + 32 <= 59) {
+              } else if (Math.round(temperature * (9/5)) + 32 > 32 && Math.round(temperature * (9/5)) + 32 <= 75) {
                 color='linear-gradient(-90deg, #fdc830, #f37335)'
-              } else if (Math.round(temperature * (9/5)) + 32 > 60) {
+              } else if (Math.round(temperature * (9/5)) + 32 > 76) {
                 color='linear-gradient(-90deg, #c21500, #ffc500)'
               }
-              $('body').css('background',color);
+              $('.container_main').css('background',color);
  }
            
       changeBgColor();
         
     }
-                             
             //This was not working, trying something else
             //     $("#temperature").html(temperatureF);
             //   }, function(){
 
             //     $("#temperature").html(temperature);
             //     });
-              
 });
 
